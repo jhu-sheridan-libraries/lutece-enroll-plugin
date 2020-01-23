8c98e626c441ca7671285ecb568e84fe703561cc
Jim Martino <james.r.martino@gmail.com>
2020-01-23 16:52:22 -0500

    add test class stubs correct unicode entry

---
+++ src/test/java/fr/paris/lutece/plugins/enroll/web/ProjectJspBeanTest.java
@@ -0,0 +1,21 @@
+package fr.paris.lutece.plugins.enroll.web;
+
+import fr.paris.lutece.test.LuteceTestCase;
+import org.junit.Ignore;
+
+@Ignore
+public class ProjectJspBeanTest extends LuteceTestCase {
+
+    public void testDoCreateProject() {
+
+    }
+
+    public void testDoRemoveProject() {
+
+    }
+
+    public void testDoModifyProject() {
+
+    }
+
+}
